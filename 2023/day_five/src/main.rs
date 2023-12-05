5a6c45ccd78f506a86e471f2bb5629fb11eaba14
lukecollier <leccollier@gmail.com>
2023-12-05 22:02:12 +0100

    2023(day5): Completed
    todo: Fun one for optimisation

---
+++ 2023/day_five/src/main.rs
@@ -0,0 +1,96 @@
+const PUZZLE_INPUT: &str = include_str!("./input.txt");
+
+fn main() {
+    one();
+    two();
+}
+
+fn one() {
+    let mut lines = PUZZLE_INPUT.lines();
+    let mut seeds = lines
+        .next()
+        .unwrap()
+        .split_once(":")
+        .unwrap()
+        .1
+        .split_ascii_whitespace()
+        .map(|s| s.parse::<usize>().unwrap())
+        .collect::<Vec<_>>();
+    let mut reading_map = false;
+    let mut conversion: Vec<(usize, usize, usize)> = Vec::new();
+    for line in lines {
+        if line.contains(':') {
+            reading_map = true;
+        } else if line.trim().is_empty() {
+            for idx in 0..seeds.len() {
+                let seed = seeds[idx];
+
+                if let Some((source_min, _, destination)) = conversion
+                    .iter()
+                    .find(|(source_min, source_max, _)| source_min <= &seed && seed <= *source_max)
+                {
+                    let add_this = source_min.abs_diff(seed);
+                    seeds[idx] = destination + add_this;
+                }
+            }
+            reading_map = false;
+            conversion.clear();
+        } else if reading_map {
+            let mut split = line.split_ascii_whitespace();
+            let destination = split.next().unwrap().parse::<usize>().unwrap();
+            let source = split.next().unwrap().parse::<usize>().unwrap();
+            let range = split.next().unwrap().parse::<usize>().unwrap();
+            conversion.push((source, source + range, destination));
+        }
+    }
+    seeds.sort();
+    println!("problem one: {}", seeds.first().unwrap());
+}
+
+fn two() {
+    let mut lines = PUZZLE_INPUT.lines();
+    let found_seeds = lines
+        .next()
+        .unwrap()
+        .split_once(":")
+        .unwrap()
+        .1
+        .split_ascii_whitespace()
+        .map(|s| s.parse::<usize>().unwrap())
+        .collect::<Vec<_>>();
+    let mut seeds = Vec::new();
+    for seed_pair in found_seeds.chunks(2) {
+        let start = seed_pair.first().unwrap().to_owned();
+        let range = seed_pair.last().unwrap().to_owned();
+        for seed in start..(start + range) {
+            seeds.push(seed);
+        }
+    }
+    let mut reading_map = false;
+    let mut conversion: Vec<(usize, usize, usize)> = Vec::new();
+    for line in lines {
+        if line.contains(':') {
+            reading_map = true;
+        } else if line.trim().is_empty() {
+            for seed in seeds.iter_mut() {
+                if let Some((source_min, _, destination)) = conversion
+                    .iter()
+                    .find(|(source_min, source_max, _)| source_min <= seed && source_max >= seed)
+                {
+                    let add_this = source_min.abs_diff(*seed);
+                    *seed = destination + add_this;
+                }
+            }
+            reading_map = false;
+            conversion.clear();
+        } else if reading_map {
+            let mut split = line.split_ascii_whitespace();
+            let destination = split.next().unwrap().parse::<usize>().unwrap();
+            let source = split.next().unwrap().parse::<usize>().unwrap();
+            let range = split.next().unwrap().parse::<usize>().unwrap();
+            conversion.push((source, source + range, destination));
+        }
+    }
+    seeds.sort();
+    println!("problem two: {}", seeds.first().unwrap());
+}
