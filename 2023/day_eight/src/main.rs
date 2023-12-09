3ee2ce7909d5dd937a47c1d27a07be85e9095ff6
Luke Collier <leccollier@gmail.com>
2023-12-09 15:52:45 +0100

    2023(day_eight): Complete

---
+++ 2023/day_eight/src/main.rs
@@ -0,0 +1,176 @@
+use num::Integer;
+use std::{collections::HashSet, rc::Rc, time::Instant};
+
+const PUZZLE_INPUT: &str = include_str!("./input.txt");
+
+fn main() {
+    // one();
+    two_numbery();
+}
+
+fn one() {
+    let mut lines = PUZZLE_INPUT.lines();
+    let mut directions = std::iter::repeat(lines.next().unwrap().chars()).flatten();
+    lines.next();
+    let new_lines = std::iter::repeat(lines).flatten();
+    let mut searching_for = "AAA";
+    let mut steps = 0;
+    for line in new_lines {
+        let instant = Instant::now();
+        let mut splits = line.split_ascii_whitespace();
+        let current = splits.next().unwrap();
+        splits.next();
+        let left_raw = splits.next().unwrap();
+        let right_raw = splits.next().unwrap();
+        let left = &left_raw[1..left_raw.len() - 1];
+        let right = &right_raw[0..left_raw.len() - 2];
+        if searching_for == current {
+            let direction = directions.next().unwrap();
+            searching_for = match direction {
+                'R' => right,
+                'L' => left,
+                _ => todo!(),
+            };
+            steps += 1;
+        }
+        println!("took {:?}", instant.elapsed());
+        if searching_for == "ZZZ" {
+            break;
+        }
+    }
+
+    println!("part one: {}", &steps);
+}
+
+struct Node<'a> {
+    label: &'a str,
+    left: Option<Rc<Node<'a>>>,
+    right: Option<Rc<Node<'a>>>,
+}
+
+impl<'a> Node<'a> {
+    fn new(label: &'a str) -> Self {
+        Node {
+            label,
+            left: None,
+            right: None,
+        }
+    }
+
+    fn insert_left(&mut self) {
+        self.left = None;
+    }
+    fn insert_right(&mut self) {
+        self.right = None;
+    }
+}
+
+fn two_naive() {
+    use std::collections::HashMap;
+
+    let mut lines = PUZZLE_INPUT.lines();
+    let mut directions = std::iter::repeat(lines.next().unwrap().chars()).flatten();
+    lines.next();
+    let mut currently_on: Vec<&str> = Vec::new();
+    let mut world: HashMap<&str, (&str, &str)> = HashMap::new();
+    let mut steps = 0;
+    // find all our starting positions
+    for line in lines {
+        let mut splits = line.split_ascii_whitespace();
+        let current = splits.next().unwrap();
+        let current_end = &current.chars().last().unwrap();
+        splits.next();
+        let left_raw = splits.next().unwrap();
+        let right_raw = splits.next().unwrap();
+        let left = &left_raw[1..left_raw.len() - 1];
+        let right = &right_raw[0..left_raw.len() - 2];
+        if current_end == &'A' {
+            currently_on.push(current);
+        }
+        world.insert(current, (left, right));
+    }
+
+    while !(&currently_on).into_iter().fold(true, |acc, str| {
+        acc && &str[str.len() - 1..str.len()] == "Z"
+    }) {
+        let direction = directions.next().unwrap();
+        steps += 1;
+        for idx in 0..currently_on.len() {
+            let (left, right) = world.get(currently_on[idx]).unwrap();
+            if direction == 'L' {
+                currently_on[idx] = left;
+            } else if direction == 'R' {
+                currently_on[idx] = right;
+            }
+        }
+        println!("took ({}) currently on: {:?}", &direction, &currently_on);
+    }
+    println!("part two: {}", steps);
+}
+
+fn two_numbery() {
+    use std::collections::HashMap;
+
+    let mut lines = PUZZLE_INPUT.lines();
+    let mut directions = std::iter::repeat(lines.next().unwrap().char_indices()).flatten();
+    lines.next();
+    let mut currently_on: Vec<&str> = Vec::new();
+    let mut world: HashMap<&str, (&str, &str)> = HashMap::new();
+    // find all our starting positions
+    for line in lines {
+        let mut splits = line.split_ascii_whitespace();
+        let current = splits.next().unwrap();
+        let current_end = &current.chars().last().unwrap();
+        splits.next();
+        let left_raw = splits.next().unwrap();
+        let right_raw = splits.next().unwrap();
+        let left = &left_raw[1..left_raw.len() - 1];
+        let right = &right_raw[0..left_raw.len() - 2];
+        if current_end == &'A' {
+            currently_on.push(current);
+        }
+        world.insert(current, (left, right));
+    }
+
+    // I think the trick is to find each time they encounter the ending Z, then using lcd or gcd we
+    // figure out how many more steps it would take us to find what we're after
+
+    // let mut steps_taken: Vec<HashSet<usize>> = Vec::with_capacity(currently_on.len());
+    let mut lcm: Option<usize> = None;
+    for idx in 0..currently_on.len() {
+        let mut steps = Vec::new();
+        let mut directions = directions.clone();
+        loop {
+            let cur = currently_on[idx];
+            let (left, right) = world.get(currently_on[idx]).unwrap();
+            let (direction_id, direction) = directions.next().unwrap();
+            if direction == 'L' {
+                currently_on[idx] = left;
+            } else if direction == 'R' {
+                currently_on[idx] = right;
+            }
+            let candidate = (cur, direction_id);
+            if steps.contains(&candidate) {
+                // we've found the loop
+                println!("loop encountered {:?} steps", steps.len());
+                break;
+            } else {
+                steps.push(candidate);
+            }
+            // now we just find the steps taken for all the z values (via index)
+            //
+            // then find the minimum least common multiple between this and the next loop wooww
+
+            // if &cur[cur.len() - 1..cur.len()] == "Z" {
+            //     break;
+            // }
+        }
+        // dbg!(steps.iter().enumerate().collect::<Vec<_>>());
+        for (idx, (cur, _)) in steps.iter().enumerate() {
+            if &cur[cur.len() - 1..cur.len()] == "Z" {
+                lcm = Some(lcm.get_or_insert(idx).lcm(&idx));
+            }
+        }
+    }
+    println!("part two: {}", lcm.unwrap());
+}
--- 2023/day_seven/src/main.rs
+++ 2023/day_seven/src/main.rs
@@ -2,10 +2,159 @@ const PUZZLE_INPUT: &str = include_str!("./input.txt");
 
 fn main() {
     one();
+    two();
 }
 
 fn one() {
-    for line in PUZZLE_INPUT.lines() {}
+    fn letter_to_strength(char: char) -> u32 {
+        match char {
+            'A' => 12,
+            'K' => 11,
+            'Q' => 10,
+            'J' => 9,
+            'T' => 8,
+            '9' => 7,
+            '8' => 6,
+            '7' => 5,
+            '6' => 4,
+            '5' => 3,
+            '4' => 2,
+            '3' => 1,
+            '2' => 0,
+            _ => panic!("OH NO"),
+        }
+    }
 
-    println!("one: {}", "");
+    fn hand_strength(cards: &str) -> u32 {
+        let mut chars = cards.chars().collect::<Vec<_>>();
+        chars.sort();
+        let mut seen: Vec<char> = Vec::new();
+        let mut score: usize = 0;
+        for char in chars.into_iter() {
+            let seen_amount = &seen
+                .clone()
+                .into_iter()
+                .filter(|look| look == &char)
+                .count();
+            score += seen_amount;
+            seen.push(char);
+        }
+        score.try_into().unwrap()
+    }
+    fn is_highest(cards: &str, other: &str) -> bool {
+        for (left, right) in cards.chars().zip(other.chars()) {
+            let strength_left = letter_to_strength(left);
+            let strength_right = letter_to_strength(right);
+            if strength_left > strength_right {
+                return true;
+            } else if strength_right > strength_left {
+                return false;
+            }
+        }
+        // this technically means a draw :shrug:
+        return false;
+    }
+    let mut ranks: Vec<(&str, usize)> = Vec::new();
+    for line in PUZZLE_INPUT.lines() {
+        if let Some((hand, bid)) = line.split_once(" ") {
+            ranks.push((hand, bid.parse::<usize>().unwrap()));
+        }
+    }
+    ranks.sort_by(|(hand_one, _), (hand_two, _)| {
+        is_highest(hand_one, hand_two).cmp(&is_highest(hand_two, hand_one))
+    });
+    ranks.sort_by(|(hand_one, _), (hand_two, _)| {
+        hand_strength(&hand_one).cmp(&hand_strength(&hand_two))
+    });
+
+    let result = &ranks
+        .iter()
+        .enumerate()
+        .fold(0, |acc, (rank_minus_one, (_, bid))| {
+            acc + ((rank_minus_one + 1) * bid)
+        });
+    println!("one: {}", result);
+}
+
+fn two() {
+    fn letter_to_strength(char: char) -> u32 {
+        match char {
+            'A' => 12,
+            'K' => 11,
+            'Q' => 10,
+            'T' => 9,
+            '9' => 8,
+            '8' => 7,
+            '7' => 6,
+            '6' => 5,
+            '5' => 4,
+            '4' => 3,
+            '3' => 2,
+            '2' => 1,
+            'J' => 0,
+            _ => panic!("OH NO"),
+        }
+    }
+    use std::collections::BTreeMap;
+    fn hand_strength(cards: &str) -> u32 {
+        let mut counts = BTreeMap::new();
+        for card in cards.chars() {
+            *counts.entry(card).or_insert(0) += 1;
+        }
+        let mut counts_as_vec = counts.into_iter().collect::<Vec<_>>();
+        counts_as_vec.sort_by(|(_, cmp1), (_, cmp2)| cmp1.cmp(&cmp2));
+
+        let first = counts_as_vec.first().unwrap();
+        let mut replace_j_with = first.0;
+        let mut chars = cards
+            .chars()
+            .map(|char| if char == 'J' { replace_j_with } else { char })
+            .collect::<Vec<_>>();
+        chars.sort();
+        let mut seen: Vec<char> = Vec::new();
+        let mut score: usize = 0;
+        for char in chars.into_iter() {
+            let seen_amount = &seen
+                .clone()
+                .into_iter()
+                .filter(|look| look == &char)
+                .count();
+            score += seen_amount;
+            seen.push(char);
+        }
+        score.try_into().unwrap()
+    }
+    fn is_highest(cards: &str, other: &str) -> bool {
+        for (left, right) in cards.chars().zip(other.chars()) {
+            let strength_left = letter_to_strength(left);
+            let strength_right = letter_to_strength(right);
+            if strength_left > strength_right {
+                return true;
+            } else if strength_right > strength_left {
+                return false;
+            }
+        }
+        // this technically means a draw :shrug:
+        return false;
+    }
+    let mut ranks: Vec<(&str, usize)> = Vec::new();
+    for line in PUZZLE_INPUT.lines() {
+        if let Some((hand, bid)) = line.split_once(" ") {
+            ranks.push((hand, bid.parse::<usize>().unwrap()));
+        }
+    }
+    ranks.sort_by(|(hand_one, _), (hand_two, _)| {
+        is_highest(hand_one, hand_two).cmp(&is_highest(hand_two, hand_one))
+    });
+    ranks.sort_by(|(hand_one, _), (hand_two, _)| {
+        hand_strength(&hand_one).cmp(&hand_strength(&hand_two))
+    });
+
+    let result = &ranks
+        .iter()
+        .enumerate()
+        .fold(0, |acc, (rank_minus_one, (_, bid))| {
+            acc + ((rank_minus_one + 1) * bid)
+        });
+    println!("two: {}", result);
 }
